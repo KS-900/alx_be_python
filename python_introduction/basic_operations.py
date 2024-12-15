95a918b4e9f0a3ee6a39e04e1a868460ae81efa2
Karabo Satekge <kb2.satekge@gmail.com>
2024-12-15 15:58:55 +0200

    Basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+result1 = number1 + number2
+result2 = number1 - number2
+result3 = number1 * number2
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is "+ str(result1))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is "+ str(result2))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is "+ str(result3))
