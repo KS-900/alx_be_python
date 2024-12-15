084a3f4d504531a167b7b106a74e4df6b31bc788
Karabo Satekge <kb2.satekge@gmail.com>
2024-12-15 16:29:37 +0200

    Error of seconds calculation

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,3 @@
 hours = 2
-seconds = 60 * 60
-hoursToSeconds = hours * seconds
-print(str(hours) + " hour(s) is " + str(hoursToSeconds) + " seconds.")
+seconds = hours * 3600
+print(str(hours) + " hour(s) is " + str(seconds) + " seconds.")
