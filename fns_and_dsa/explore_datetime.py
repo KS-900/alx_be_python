2d00b77d5bb84b751b1123e41f02ac05c20d9365
Karabo Satekge <kb2.satekge@gmail.com>
2025-01-16 22:57:39 +0200

    Date formmating fix fr explore datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,5 +6,6 @@ def display_current_datetime():
 number_of_days = int(input("Enter the number of days to add to the current date: "))
 def calculate_future_date():
     future_date = datetime.now() + timedelta(days=number_of_days)
+    future_date = future_date.strftime(("%Y-%m-%d"))
     print(f"Future date: {future_date}")
 calculate_future_date()
