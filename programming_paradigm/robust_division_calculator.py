200fdbeb1a4772189b7e885e394adb65e3f1adaa
Karabo Satekge <kb2.satekge@gmail.com>
2025-01-24 17:08:11 +0200

    error fixed

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,6 +5,6 @@ def safe_divide(numerator, denominator):
         result = numerator / denominator
         return f"Result: {result:.2f}"
     except ZeroDivisionError:
-        return "Error: Division by zero is not allowed."
+        return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error Both inputs must be numeric."
+        return "Error: Please enter numric values only." 
